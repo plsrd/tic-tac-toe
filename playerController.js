064a8d28be34f1f88ffb926c0c43bc95a53b996f
racheallarimer <racheal.larimer@gmail.com>
2020-12-28 09:34:41 -0800

    add initHumanOpponent function

--- playerController.js
+++ playerController.js
@@ -1,3 +1,21 @@
 (function () {
 
+  const players = {
+    bindEvents: () => {
+      events.on('humanOpponent', players.initHumanOpponent);
+    },
+
+    createPlayer: () => {
+      return { moves: [],}
+    },
+
+    initHumanOpponent: () => {
+      players.playerOne = players.createPlayer();
+      players.playerTwo = players.createPlayer();
+      console.log(players)
+    },
+
+  }
+
+  players.bindEvents();
 })()
