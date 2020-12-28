6d2a236b63e7561de8df04a9753912a023b865d7
racheallarimer <racheal.larimer@gmail.com>
2020-12-28 08:57:31 -0800

    add player selector functions

--- displayController.js
+++ displayController.js
@@ -1,44 +1,67 @@
 (function() {
 
-  init = () => {
-    this.drawBoard();
-  }
+  const display = {
 
-  cacheDom = () => {
-    this.gameDisplay = document.getElementById('gameDisplay');
-    this.infoDisplay = document.getElementById('infoDisplay');
-    this.gameGrid = document.getElementById('gameGrid');
-  }
+    init: () => {
+      display.drawPlayerSelector();
+    },
 
-  bindEvents = () => {
-    events.on('startGame', init);
-  }
-  createDiv = (id, target) => {
-    const div = document.createElement('div');
-    div.setAttribute('id', id);
-    target.appendChild(div);
-  }
+    bindEvents: () => {
+      events.on('startGame', display.init);
+    },
 
-  drawBoard = () => {
-  //create display
-    createDiv('gameDisplay', main);
-    cacheDom();
-    createDiv('infoDisplay', gameDisplay);
-    createDiv('gameGrid', gameDisplay)
-    cacheDom();
-  //append 2 divs to display: info and game board
-  //Draw tic-tac-toe grid in game board
-    for (let i = 1; i <= 9; i++) {
-      if (i <= 3){
-        createDiv(`A${i}`, gameGrid);
-      } else if (i > 3 && i <= 6) {
-        createDiv(`B${i - 3}`, gameGrid);
-      } else {
-        createDiv(`C${i - 6}`, gameGrid);
+    createElement: (id, target, type) => {
+      const el = document.createElement(type);
+      el.setAttribute('id', id);
+      target.appendChild(el);
+    },
+
+    drawBoard: () => {
+      display.createElement('gameGrid', gameDisplay, 'div')
+      for (let i = 1; i <= 9; i++) {
+        if (i <= 3){
+          display.createElement(`A${i}`, gameGrid, 'div');
+        } else if (i > 3 && i <= 6) {
+          display.createElement(`B${i - 3}`, gameGrid, 'div');
+        } else {
+          display.createElement(`C${i - 6}`, gameGrid, 'div');
+        }
       }
-    }
+    },
+
+    drawPlayerSelector: () => {
+      display.createElement('gameDisplay', main, 'div');
+      display.createElement('playerSelect', gameDisplay, 'p');
+      playerSelect.textContent = 'select game';
+      display.createElement('humanButton', gameDisplay, 'button');
+      humanButton.textContent = 'human v human';
+      display.createElement('computerButton', gameDisplay, 'button');
+      computerButton.textContent = 'human v computer';
+      display.playerSelectorEvents();
+    },
+
+    playerSelectorEvents: () => {
+      computerButton.addEventListener('click', () => {
+        events.emit('computerOpponent');
+        display.clearDisplay(gameDisplay);
+        display.drawBoard();
+      });
+
+      humanButton.addEventListener('click', () => {
+        events.emit('humanOpponent');
+        display.clearDisplay(gameDisplay);
+        display.drawBoard();
+      });
+      
+    },
+
+    clearDisplay: (el) => {
+      while (el.firstChild) {
+        el.removeChild(el.firstChild);
+      }
+    },
   }
 
-  bindEvents();
+  display.bindEvents();
 
 })();
